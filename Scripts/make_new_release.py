a4cd0b0b0f803329cc4923b6b4568c68906838e6
Randall Lowe <72294336+rslowe@users.noreply.github.com>
2025-04-03 13:26:47 -0700

    Migrate LooseVersion / StrictVersion usage to packaging.version (PEP 632)

--- Scripts/make_new_release.py
+++ Scripts/make_new_release.py
@@ -29,12 +29,12 @@ import tempfile
 import urllib.error
 import urllib.parse
 import urllib.request
-from distutils.version import LooseVersion
 from pprint import pprint
 from shutil import rmtree
 from time import strftime
 
 import certifi
+from packaging.version import parse
 
 
 class GitHubAPIError(BaseException):
@@ -199,7 +199,7 @@ def main():
     except BaseException:
         sys.exit("Couldn't determine current autopkg version!")
     print(f"** Current AutoPkg version: {current_version}")
-    if LooseVersion(next_version) <= LooseVersion(current_version):
+    if parse(next_version) <= parse(current_version):
         sys.exit(
             f"Next version (gave {next_version}) must be greater than current version "
             f"{current_version}!"
